3fcdc333566eb30652bafed960bfa702b7726ae0
Adam Woods <adam.woods13@icloud.com>
2020-03-17 13:51:17 +0000

    Data Analysis
    Implemented a quick bar chart to visualise the balance of data, the data is very unbalanced towards real messages

--- filter.py
+++ filter.py
@@ -2,7 +2,7 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+@author: adamw
 """
 
 import pandas as pd
@@ -15,17 +15,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
+
 pd.set_option('display.max_colwidth', 1000)
 
 dataframe = pd.read_csv("Data/sms_messages.csv")
-dataframe.head(10)
 
 df_label = dataframe['Category']
 df_features = dataframe.drop('Category', 1)
-print(df_label.head())
-df_features.head()
+print(df_label.head(10))
+
+X_train, X_test, y_train, y_test = train_test_split(dataframe.Message, dataframe.Category, test_size=0.5, random_state = 0)
 
 text_pipe = Pipeline([
     ('vect', CountVectorizer(stop_words='english')),
@@ -33,12 +32,15 @@ text_pipe = Pipeline([
     ('clf', MultinomialNB()),
 ])
 
-text_pipe.fit(dataframe.Message, dataframe.Category);
+text_pipe.fit(X_train, y_train);
+print("Training score: ", text_pipe.score(X_train, y_train))
+print("Testing score:  ", text_pipe.score(X_test, y_test))
+
 
 X_test = [
     "Winner!",
-    "Hi, are you okay?",
-    "Make an appointment with me soon please",
-    "Answer these qustions to get a prize",
+    "Hate this app!",
+    "Click here now",
+    "This doesn't work super well",
 ]
 print(text_pipe.predict(X_test))
